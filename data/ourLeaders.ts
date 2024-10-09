5291071dc722af5e68102a05de7d2bca55bd506b
Touza Isaac <62522441+Touza-Isaac@users.noreply.github.com>
2024-10-09 16:12:00 +0100

    changement du design de la section our leaders

--- data/ourLeaders.ts
+++ data/ourLeaders.ts
@@ -3,6 +3,10 @@ import leader2 from '../assets/images/leaders/leader2.png'
 import leader3 from '../assets/images/leaders/leader3.png'
 import leader4 from '../assets/images/leaders/leader4.png'
 import leader5 from '../assets/images/leaders/leader5.png'
+import sanda from '../assets/images/leaders/sanda.jpg'
+import yameni from '../assets/images/leaders/nestor.jpg'
+import bekaiwe from '../assets/images/leaders/bekaiwe.jpg'
+
 import OurLeaders from '@/interfaces/ourLeaders'
 
 
@@ -10,46 +14,90 @@ const leaders:OurLeaders[] = [
     {
         name: "Touza Isaac",
         url: leader5,
+        role:"Coordonateur",
         socials:{
-            gmail: "johndoe@gmail.com",
-            facebook: "aajt",
-            linkedln: ""
+            gmail: "touzaisaac3@gmail.com",
+            facebook: "https://www.facebook.com/touza.isaac",
+            linkedln: "",
+            github: ""
         }
     },
     {
-        name: "",
+        name: "Dr Saoungoumi Rodrigue",
+        role:"Président d'honneur",
         url: leader4,
         socials:{
             gmail: "johndoe@gmail.com",
             facebook: "aajt",
-            linkedln: ""
+            linkedln: "",
+            github: ""
         }
     },
     {
-        name: "",
-        url: leader1,
-        socials:{
-            gmail: "johndoe@gmail.com",
-            facebook: "aajt",
-            linkedln: ""
-        }
-    },
-    {
-        name: "",
-        url: leader2,
-        socials:{
-            gmail: "johndoe@gmail.com",
-            facebook: "aajt",
-            linkedln: ""
-        }
-    },
-    {
-        name: "",
+        name: "Nsangou Assan Zidan",
+        role:"Leader -Adamaoua",
         url: leader3,
         socials:{
             gmail: "johndoe@gmail.com",
             facebook: "aajt",
-            linkedln: ""
+            linkedln: "",
+            github: ""
+        }
+    },
+    {
+        name: "Yameni Pougoue Gabin Wilfried",
+        role:"Co-Leader -Adamaoua",
+        url: yameni,
+        socials:{
+            gmail: "johndoe@gmail.com",
+            facebook: "aajt",
+            linkedln: "",
+            github: ""
+        }
+    },
+    {
+        name: "SANDA Oumarou",
+        role:"Leader - Extrême Nord",
+        url: sanda,
+        socials:{
+            gmail: "johndoe@gmail.com",
+            facebook: "aajt",
+            linkedln: "",
+            github: ""
+        }
+    },
+
+    {
+        name: "VANA ZOKOM ELIE",
+        role:"Co-Leader - Extrême Nord",
+        url: leader1,
+        socials:{
+            gmail: "johndoe@gmail.com",
+            facebook: "aajt",
+            linkedln: "",
+            github: ""
+        }
+    },
+    {
+        name: "Bakaiwe Menga Rodrigue",
+        role:"Leader - Nord",
+        url: bekaiwe,
+        socials:{
+            gmail: "johndoe@gmail.com",
+            facebook: "aajt",
+            linkedln: "",
+              github: ""
+        }
+    },
+    {
+        name: "DJAKAYE Dieudonné ",
+        role:"Co-Leader - Nord",
+        url: leader2,
+        socials:{
+            gmail: "johndoe@gmail.com",
+            facebook: "aajt",
+            linkedln: "",
+              github: ""
         }
     }
 ]
--- interfaces/ourLeaders.ts
+++ interfaces/ourLeaders.ts
@@ -2,10 +2,12 @@ import { StaticImageData } from "next/image";
 
 export default interface OurLeaders {
     name: string;
+    role: string;
     socials: {
         gmail: string;
         facebook: string;
         linkedln: string;
+        github: string;
     }
     url: StaticImageData;
   }
