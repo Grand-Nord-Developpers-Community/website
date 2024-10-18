a66713249461fef9ae6b84ea1ce98ad60fe534af
OumarouSandaSouley <oumarousandasouleyofficial@gmail.com>
2024-10-18 09:07:36 +0100

    feat: add email sending

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,51 +1,145 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Phone, Mail, MapPin } from "lucide-react";
 import Link from "next/link";
+import emailjs from "emailjs-com";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const ContactUs = () => {
+const ContactUs: React.FC = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const [isSending, setIsSending] = useState(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const sendEmail = (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsSending(true);
+
+    const serviceID = "service_jsgqqtb"; // Replace with environment varibales later on deployment
+    const templateID = "template_5fsocvq"; // Replace with environment varibales later on deployment
+    const publicKey = "ODliIIToK3nyFsqod"; // Replace with environment varibales later on deployment
+
+    emailjs
+      .send(serviceID, templateID, formData, publicKey)
+      .then(
+        (response) => {
+          console.log(
+            "Email successfully sent!",
+            response.status,
+            response.text
+          );
+          toast.success("Nous avons reçu votre message avec succès!");
+          setFormData({ name: "", email: "", phone: "", message: "" });
+        },
+        (error) => {
+          console.error("Failed to send the email. Error:", error);
+          toast.error(
+            "Une erreur s'est produite. Veuillez recommencer plutard."
+          );
+        }
+      )
+      .finally(() => {
+        setIsSending(false);
+      });
+  };
+
   return (
     <div className="flex flex-col">
+      <ToastContainer />
       <div className="bg-primary text-white">
-        <div className="container mx-auto px-4 py-8 relative"> {/* Ajout de relative pour le positionnement absolu */}
+        <div className="container mx-auto px-4 py-8 relative">
           <div className="flex flex-col lg:flex-row gap-8">
-            {/* Côté gauche */}
             <div className="w-full lg:w-1/2">
-              <h1 className="text-4xl font-bold mb-4">Contactez-nous</h1>
+              <h1 className="text-4xl font-bold mb-4">Contact Us</h1>
               <p className="text-lg">
-                N&apos;hésitez pas à nous contacter ? Soumettez vos questions ici et nous vous répondrons dans les plus brefs délais.
+                Feel free to contact us! Submit your questions here, and we’ll
+                get back to you as soon as possible.
               </p>
             </div>
-
-            {/* Côté droit */}
-            <div className="w-full lg:w-1/2 relative"> {/* relative pour positionnement de Card */}
+            <div className="w-full lg:w-1/2 relative">
               <Card className="w-full bg-white shadow-lg rounded-lg overflow-hidden">
                 <CardContent className="p-4">
-                  <h2 className="text-2xl font-semibold text-gray-800 mb-6">Envoyez-nous un message</h2>
+                  <h2 className="text-2xl font-semibold text-gray-800 mb-6">
+                    Send us a message
+                  </h2>
                 </CardContent>
               </Card>
 
-              {/* Formulaire positionné absolument */}
-              <form className="absolute w-full p-8 bg-white shadow-lg rounded-lg top-12 left-0 space-y-6"> {/* positionnement absolu */}
-                <Input placeholder="Nom" className="w-full" />
-                <Input type="email" placeholder="Email" className="w-full" />
-                <Input type="tel" placeholder="Téléphone" className="w-full" />
-                <Textarea placeholder="Message" className="w-full" rows={4} />
-                <Button className="w-full bg-primary text-white hover:bg-primary-dark">Envoyer</Button>
+              <form
+                className="absolute w-full p-8 bg-white shadow-lg rounded-lg top-12 left-0 space-y-6"
+                onSubmit={sendEmail}
+              >
+                <Input
+                  name="name"
+                  placeholder="Name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="w-full text-gray-800"
+                  required
+                />
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-full text-gray-800"
+                  required
+                />
+                <Input
+                  type="tel"
+                  name="phone"
+                  placeholder="Phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  className="w-full text-gray-800"
+                  required
+                />
+                <Textarea
+                  name="message"
+                  placeholder="Message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  className="w-full text-gray-800"
+                  rows={4}
+                  required
+                />
+                <Button
+                  type="submit"
+                  className="w-full bg-primary text-white hover:bg-primary-dark"
+                  disabled={isSending}
+                >
+                  {isSending ? "Sending..." : "Send"}
+                </Button>
               </form>
             </div>
           </div>
         </div>
       </div>
+      {/* Extra spacing for mobile */}
+      <div className="h-80 md:hidden"></div>
 
-       {/* Ajout d'un espacement supplémentaire uniquement en mode mobile */}
-       <div className="h-80 md:hidden"></div> 
-      
-      {/* Coordonnées et icônes de médias sociaux */}
-      <div className="container mx-auto px-4 py-12 ">
+      {/* Contact details and social media icons */}
+      <div className="container mx-auto px-4 py-12">
         <div className="flex flex-col lg:flex-row gap-8">
           <div className="w-full lg:w-1/2 space-y-8">
             <div className="space-y-4">
@@ -64,33 +158,16 @@ const ContactUs = () => {
             </div>
             <div className="flex space-x-4">
               <Link href="#" className="text-primary hover:text-primary-dark">
-                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
-                </svg>
-              </Link>
-              <Link href="#" className="text-primary hover:text-primary-dark">
-                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path fillRule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clipRule="evenodd" />
-                </svg>
-              </Link>
-              <Link href="#" className="text-primary hover:text-primary-dark">
-                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                </svg>
-              </Link>
-              <Link href="#" className="text-primary hover:text-primary-dark">
-                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
-                </svg>
+                {/* Social media icons */}
               </Link>
             </div>
           </div>
         </div>
       </div>
-      
-      {/* Carte */}
+
+      {/* Map */}
       <div className="container mx-auto px-4 py-10">
-        <h2 className="text-2xl font-semibold mb-4">Notre localisation</h2>
+        <h2 className="text-2xl font-semibold mb-4">Our Location</h2>
         <div className="h-64 rounded-lg overflow-hidden">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15686.172842329875!2d14.3226566!3d10.5951428!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTDCsDM1JzQ0LjUiTiAxNMKwMTknMjMuNiJF!5e0!3m2!1sen!2scm!4v1696264521879!5m2!1sen!2scm"
