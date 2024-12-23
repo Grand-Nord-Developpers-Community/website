2d053060ac132612eed4527a223a59faeac8cec5
mohamedelbachir <mohamedelbachirboubanganadakou@gmail.com>
2024-12-23 10:57:00 +0100

    fix add clearly progressURL supabase on file (to be fix)

--- lib/db.ts
+++ lib/db.ts
@@ -2,8 +2,8 @@ import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
 
 import * as schema from "./schema"
-
-const pool = postgres(process.env.DB_URL!, { max: 1 })
+//to be fix
+const pool = postgres("postgresql://postgres.yzbnqnrwsximakxfdqhi:8HDNqBzIUvAbudYo@aws-0-eu-central-1.pooler.supabase.com:6543/postgres", { max: 1 })
 
 export const db = drizzle(pool, {
   schema,
