30a981dc6ed3a4282ac5937557599fc2dff187ce
mohamedelbachir <mohamedelbachirboubanganadakou@gmail.com>
2024-12-25 21:39:32 +0100

    fix:add revalidate blog list page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,6 +10,7 @@ import { Button } from "@/components/ui/button";
 import EmptyBlog from "@/assets/svgs/undraw_add_notes_re_ln36.svg";
 import ErrorComponent from "@/components/errorComponent";
 import Link from "next/link";
+export const revalidate=0;
 const BlogPage = () => {
   const { data: blogs, isLoading, isError } = useGetListBlog();
   return (
