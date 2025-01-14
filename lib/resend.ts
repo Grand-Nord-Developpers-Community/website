6fb33b0fc6382d70c6271078fa51ce122acb2061
mohamedelbachirboubanganadakou <mohamedelbachirboubanganadakou@gmail.com>
2025-01-14 00:45:39 +0000

    fix:resend api

--- lib/resend.ts
+++ lib/resend.ts
@@ -1,3 +1,8 @@
 import { Resend } from "resend";
+import * as dotenv from "dotenv";
 
+dotenv.config({
+  path: ".env",
+});
+console.log(process.env.RESEND_API_KEY)
 export const resend = new Resend(process.env.RESEND_API_KEY);
