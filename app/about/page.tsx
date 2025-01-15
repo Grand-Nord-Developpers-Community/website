e200a8c82bdeabd26faf7100613e957ec28c3ed0
Oumarou Sanda Souley <148893596+OumarouSandaSouley@users.noreply.github.com>
2025-01-15 17:49:50 +0100

    update: enhance about us page  design

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,126 +1,214 @@
-import React from 'react';
-import { Card, CardHeader, CardContent } from '@/components/ui/card';
-import { Code, Users, Target, Activity, Briefcase, PhoneCall } from 'lucide-react';
+import React from "react";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Metadata } from "next";
+import HeadingPage from "@/sections/common/HeadingPage";
+import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
+import {
+  Code,
+  Users,
+  Target,
+  Activity,
+  Briefcase,
+  PhoneCall,
+  Globe,
+  Zap,
+  BookOpen,
+  Rocket,
+} from "lucide-react";
 
 export const metadata: Metadata = {
-  title: "GNDC | A propos",
+  title: "GNDC | À propos de nous",
   description:
-    "Communauté technologique pour la promotion de l'innovation et de la technologie dans le Grand Nord Cameroun",
+    "Rejoignez la Grand Nord Developers Community (GNDC) pour stimuler l'innovation technologique et le développement dans le nord du Cameroun. Découvrez notre mission, nos activités et comment vous pouvez participer à cette aventure passionnante.",
+  keywords:
+    "GNDC, développeurs, Cameroun, Grand Nord, technologie, innovation, communauté",
 };
+
 const AboutUs = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold mb-8 text-center">A Propos de GNDC</h1>
-      <hr/>
-      <section className="mb-12 py-8">
-        <h2 className="text-2xl font-semibold mb-4 flex items-center">
-          <Code className="mr-2" /> Description
-        </h2>
-        <p className="mb-4">
-          Grand Nord Developers Community (GNDC) est une communauté dynamique et passionnée de développeurs basée dans le Grand Nord du Cameroun. Cette communauté est composée de développeurs, ingénieurs et passionnés de technologie qui partagent une vision commune de l&apos;avenir technologique de leur région.
-        </p>
-        <p>Notre devise : Nous contribuons au développement technologique du Grand Nord Cameroun</p>
-      </section>
-
-      <section className="mb-12">
-        <h2 className="text-2xl font-semibold mb-4 flex items-center">
-          <Target className="mr-2" /> Nos Objectifs
-        </h2>
-        <ul className="list-disc pl-6">
-          <li>Stimuler l&apos;innovation technologique</li>
-          <li>Favoriser la collaboration entre les développeurs</li>
-          <li>Contribuer au développement de la région et du Cameroun</li>
-          <li>Encourager la collaboration entre les développeurs locaux pour bâtir un réseau solide et solidaire</li>
-          <li>Fournir des ressources et des formations afin de développer les compétences des membres et de les maintenir à jour avec les dernières avancées technologiques</li>
-          <li>Créer un écosystème favorable à l&apos;innovation et à l&apos;entrepreneuriat, permettant aux développeurs de transformer leurs idées en projets concrets</li>
-        </ul>
-      </section>
-
-      <section className="mb-12">
-        <h2 className="text-2xl font-semibold mb-4 flex items-center">
-          <Activity className="mr-2" /> Nos Activités
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <Card>
-            <CardHeader className="flex items-center">
-              <Users className="mr-2" /> Formations et Ateliers
-            </CardHeader>
-            <CardContent>
-              <ul className="list-disc pl-6">
-                <li>Organiser des formations pour initier les jeunes aux domaines de la technologie</li>
-                <li>Ateliers pratiques pour découvrir les processus technologiques</li>
-                <li>Masterclass et séminaires sur l&apos;entrepreneuriat et autres compétences avancées</li>
-              </ul>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex items-center">
-              <Code className="mr-2" /> Hackathons et Compétitions de Codage
-            </CardHeader>
-            <CardContent>
-              <ul className="list-disc pl-6">
-                <li>Organiser des hackathons nocturnes pour travailler sur des projets intensifs</li>
-                <li>Projets de codage collaboratifs axés sur des défis spécifiques ou des initiatives communautaires</li>
-                <li>Défis de codage en direct avec des prix pour les gagnants</li>
-              </ul>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex items-center">
-              <Users className="mr-2" /> Mentorat et Opportunités Professionnelles
-            </CardHeader>
-            <CardContent>
-              <ul className="list-disc pl-6">
-                <li>Faciliter les interactions régulières entre mentors expérimentés et apprenants</li>
-                <li>Offrir des opportunités de stage, des programmes de formation et des événements de recrutement</li>
-                <li>Faciliter la connexion entre développeurs et professionnels de l&apos;industrie</li>
-              </ul>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex items-center">
-              <Briefcase className="mr-2" /> Soutien aux Start-ups et Partenariats
-            </CardHeader>
-            <CardContent>
-              <ul className="list-disc pl-6">
-                <li>Masterclass certifiantes pour les aspirants entrepreneurs</li>
-                <li>Accès à des ressources supplémentaires et occasions d&apos;apprentissage grâce aux partenariats</li>
-                <li>Soutenir les initiatives technologiques locales et promouvoir l&apos;entrepreneuriat technologique</li>
-              </ul>
-            </CardContent>
-          </Card>
-        </div>
-      </section>
-
-      <section className="mb-12">
-        <h2 className="text-2xl font-semibold mb-4 flex items-center">
-          <Users className="mr-2" /> Pourquoi Rejoindre GNDC ?
-        </h2>
-        <ul className="list-disc pl-6">
-          <li>Possibilité de participer à des formations, des ateliers pratiques et des sessions de mentorat</li>
-          <li>Opportunités de collaboration et de partage de connaissances avec d&apos;autres développeurs passionnés</li>
-          <li>Accès à des ressources et à des opportunités de formation pour améliorer ses compétences techniques</li>
-          <li>Possibilités de développement professionnel et personnel à travers divers programmes et événements organisés par la communauté</li>
-        </ul>
-      </section>
-
-      <section className="mb-12">
-        <h2 className="text-2xl font-semibold mb-4 flex items-center">
-          <PhoneCall className="mr-2" /> Contactez-nous
-        </h2>
-        <p>Téléphone : +237 691805321 / +237 672277579</p>
-        <p>Email : contact@gndc.org</p>
-
-        <div className="mt-6 flex justify-center space-x-4">
-            <Button className="text-white" asChild>
-                <Link href="/contact"> Laissez un message</Link>
-             </Button>
+    <section className="w-full bg-gradient-to-b from-gray-50 to-white">
+      <HeadingPage
+        title="Qui sommes-nous"
+        description="A propos de la Grand Nord Developers Community"
+        icon={<QuestionMarkCircledIcon className="w-8 h-8 text-white" />}
+      />
+      <div className="container mx-auto px-4 py-12">
+        <section className="mb-16 py-8">
+          <h2 className="text-3xl font-bold mb-6 text-center text-primary">
+            Notre Vision
+          </h2>
+          <p className="text-lg text-center max-w-3xl mx-auto leading-relaxed">
+            À la GNDC, nous sommes plus qu'une communauté - nous sommes un
+            mouvement. Notre passion pour la technologie et l'innovation nous
+            pousse à remodeler l'avenir du Grand Nord du Cameroun. Ensemble,
+            nous construisons un écosystème dynamique où les idées fleurissent,
+            les compétences se développent et les rêves deviennent réalité.
+          </p>
+          <div className="mt-8 text-center">
+            <Link
+              href="/bemember"
+              className="bg-primary hover:bg-primary-dark text-white font-semibold py-3 px-6 rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg"
+            >
+              Participer à la révolution
+            </Link>
           </div>
-      </section>
-    </div>
+        </section>
+
+        <section className="mb-16">
+          <h2 className="text-3xl font-bold mb-8 text-center text-primary">
+            Ce Qui Nous Anime
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {[
+              {
+                icon: <Zap className="w-10 h-10 text-yellow-500" />,
+                title: "Innovation",
+                description: "Favoriser des idées et des solutions novatrices",
+              },
+              {
+                icon: <Users className="w-10 h-10 text-blue-500" />,
+                title: "Collaboration",
+                description: "Construire un réseau solide et solidaire",
+              },
+              {
+                icon: <BookOpen className="w-10 h-10 text-green-500" />,
+                title: "Éducation",
+                description:
+                  "Apprentissage continu et développement des compétences",
+              },
+              {
+                icon: <Rocket className="w-10 h-10 text-red-500" />,
+                title: "Croissance",
+                description: "Accélérer le développement personnel et régional",
+              },
+              {
+                icon: <Target className="w-10 h-10 text-purple-500" />,
+                title: "Impact",
+                description:
+                  "Faire une différence durable dans notre communauté",
+              },
+              {
+                icon: <Briefcase className="w-10 h-10 text-indigo-500" />,
+                title: "Opportunité",
+                description: "Créer des voies vers le succès professionnel",
+              },
+            ].map((item, index) => (
+              <Card
+                key={index}
+                className="bg-white shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out"
+              >
+                <CardHeader className="flex flex-col items-center">
+                  {item.icon}
+                  <h3 className="text-xl font-semibold mt-4">{item.title}</h3>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-center">{item.description}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </section>
+
+        <section className="mb-16">
+          <h2 className="text-3xl font-bold mb-8 text-center text-primary">
+            Nos Initiatives
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            {[
+              {
+                icon: <Code className="w-8 h-8 text-primary" />,
+                title: "Ateliers Techniques & Bootcamps",
+                description:
+                  "Des expériences d'apprentissage intensives et pratiques pour améliorer vos compétences",
+              },
+              {
+                icon: <Activity className="w-8 h-8 text-primary" />,
+                title: "Hackathons & Défis de Programmation",
+                description:
+                  "Compétitions excitantes pour résoudre des problèmes réels",
+              },
+              {
+                icon: <Users className="w-8 h-8 text-primary" />,
+                title: "Programme de Mentorat",
+                description:
+                  "Orientation par des experts de l'industrie pour accélérer votre croissance",
+              },
+              {
+                icon: <Briefcase className="w-8 h-8 text-primary" />,
+                title: "Incubateur de Startups",
+                description:
+                  "Support et ressources pour transformer vos idées en projets réussis",
+              },
+            ].map((item, index) => (
+              <Card
+                key={index}
+                className="bg-white shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out"
+              >
+                <CardHeader className="flex items-center space-x-4">
+                  {item.icon}
+                  <h3 className="text-xl font-semibold">{item.title}</h3>
+                </CardHeader>
+                <CardContent>
+                  <p>{item.description}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </section>
+
+        <section className="mb-16 bg-primary text-white rounded-lg p-8">
+          <h2 className="text-3xl font-bold mb-6 text-center">
+            Rejoignez nous !
+          </h2>
+          <p className="text-lg text-center mb-8">
+            Faites partie d'une communauté qui façonne l'avenir de la
+            technologie dans le Grand Nord. Il y a une place pour vous ici.
+          </p>
+          <div className="flex justify-center space-x-4">
+            <Button variant="secondary" size="lg" asChild>
+              <Link href="/bemember">Nous rejoindre</Link>
+            </Button>
+            <Button
+              variant="default"
+              className="border-2 border-secondary"
+              size="lg"
+              asChild
+            >
+              <Link href="/events">Événements à Venir</Link>
+            </Button>
+          </div>
+        </section>
+
+        <section className="text-center">
+          <h2 className="text-3xl font-bold mb-6 text-primary">
+            Nous contacter
+          </h2>
+          <p className="mb-4">
+            <PhoneCall className="inline-block mr-2" /> +237 691 805 321 / +237
+            672 277 579
+          </p>
+          <p className="mb-6">
+            <Globe className="inline-block mr-2" /> contact@gndc.org
+          </p>
+          <div className="flex justify-center space-x-4">
+            <Button variant="outline" size="lg" asChild>
+              <Link href="/contact">Envoyer un Message</Link>
+            </Button>
+            <Button variant="outline" size="lg" asChild>
+              <a
+                href="https://twitter.com/GNDCommunity"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Nous suivre
+              </a>
+            </Button>
+          </div>
+        </section>
+      </div>
+    </section>
   );
 };
 
