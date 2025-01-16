20046527965350a9746ce3e193123a37d46cb431
mohamedelbachirboubanganadakou <mohamedelbachirboubanganadakou@gmail.com>
2025-01-16 11:17:52 +0000

    fix:theme-color

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,7 +81,7 @@ export default async function RootLayout({
       >
         <HeaderWrapper />
         <main className="w-full min-h-screen overflow-x-clip">
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeProvider attribute="class" defaultTheme="light">
             <SessionProvider session={session}>
               <ConfirmDialogProvider>
                 <TooltipProvider>
