521c4d1d09fe2bdbcf4df46fbc3913d022600308
mohamedelbachir <mohamedelbachirboubanganadakou@gmail.com>
2025-09-21 19:48:48 +0100

    add wrangler.jsonc

--- lib/rate-limiting.ts
+++ lib/rate-limiting.ts
@@ -1,18 +1,10 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
-import * as dotenv from "dotenv";
-
-dotenv.config({
-  path: ".env",
-});
 // Create a new ratelimiter, that allows 5 requests per 30 seconds
 export const ratelimiter =
   process.env.IS_RATE_LIMIT_ENABLED === "false"
     ? undefined
     : new Ratelimit({
-        redis: new Redis({
-          url: process.env.UPSTASH_REDIS_REST_URL,
-          token: process.env.UPSTASH_REDIS_REST_TOKEN,
-        }),
+        redis: Redis.fromEnv(),
         limiter: Ratelimit.slidingWindow(5, "30 s"),
       });
