4fe7a60202da5af79e8592f742b6c8b4c1c02f32
mohamedelbachir <mohamedelbachirboubanganadakou@gmail.com>
2025-12-06 14:31:47 +0100

    try this way

--- netlify/edge-functions/preview-image.tsx
+++ netlify/edge-functions/preview-image.tsx
@@ -56,33 +56,33 @@ export default async (request: Request, context: Context) => {
   // console.log("Image response status:", image.status);
   // console.log("Image response status text:", image.statusText);
   console.log("Returning image response");
-  // return new ImageResponse(image.contentToRender, {
-  //   width: 1200,
-  //   height: 630,
-  //   fonts: image.fonts,
-  // });
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 128,
-          background: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          textAlign: "center",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        Hello world!
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 600,
-    }
-  );
+  return new ImageResponse(image.contentToRender, {
+    width: 1200,
+    height: 630,
+    fonts: image.fonts,
+  });
+  // return new ImageResponse(
+  //   (
+  //     <div
+  //       style={{
+  //         fontSize: 128,
+  //         background: "white",
+  //         width: "100%",
+  //         height: "100%",
+  //         display: "flex",
+  //         textAlign: "center",
+  //         alignItems: "center",
+  //         justifyContent: "center",
+  //       }}
+  //     >
+  //       Hello world!
+  //     </div>
+  //   ),
+  //   {
+  //     width: 1200,
+  //     height: 600,
+  //   }
+  // );
 };
 
 export const config: Config = { path: "/preview-image/:slug" };
