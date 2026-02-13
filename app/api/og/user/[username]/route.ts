5270b775e799ce0dbdf943d16e2155d4de6a86e1
mohamedelbachir <mohamedelbachirboubanganadakou@gmail.com>
2026-02-13 20:21:23 +0100

    fix user og image

--- app/api/og/user/[username]/route.ts
+++ app/api/og/user/[username]/route.ts
@@ -3,6 +3,15 @@ import { generateOgImageResponse } from "../../og";
 import { getForumPost } from "@/actions/forum.actions";
 import { formatRelativeTime } from "@/lib/utils";
 import { getUserProfileMeta } from "@/actions/user.actions";
+
+function bio(bio: string | null) {
+  const charsPerLine = 50;
+  const maxChars = charsPerLine * 2.5;
+  if (bio && bio.length > maxChars) {
+    return bio.slice(0, maxChars - 1) + "…";
+  }
+  return bio || "Pas de bio!";
+}
 export async function GET(
   request: NextRequest,
   { params }: { params: Promise<{ username: string }> },
@@ -12,6 +21,7 @@ export async function GET(
   if (!user) {
     return new Response("Not found", { status: 404 });
   }
+
   const date = formatRelativeTime(user.createdAt);
   const p = new URLSearchParams({
     templateId: "E0fh07i6JOACradERRWC8",
@@ -20,9 +30,9 @@ export async function GET(
       `${process.env.BASE_URL}/api/avatar?username=${user?.username}`,
     "name-i5n2gabp7": user?.name || "",
     "username-3zf3ntmy6": "@ " + user?.username,
-    "bio-e8n7pme45": user?.bio || "Pas de bio !",
+    "bio-e8n7pme45": bio(user.bio),
     "xp-g5uu8lpx0": `${user?.experiencePoints || 0} xp`,
-    "streak-d6mamky2c": `${user.activity?.activity || 0} jours`,
+    "streak-d6mamky2c": `${user.activity?.totalDaysActive || 0} jours`,
     "questioncount-jwknfkx19": `${user.blogPosts.length} discussions`,
     "blogcount-qwco488oz": `${user.blogPosts.length} blogs`,
   });
